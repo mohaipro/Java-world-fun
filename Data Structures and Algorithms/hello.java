1524f31829efac067926610ff80d1817b763e8ef
mohaipro <112693316+mohaipro@users.noreply.github.com>
2025-06-28 20:36:56 +0300

    Create hello.java
    first file for learning java (data structures and algorithms)

---
+++ Data Structures and Algorithms/hello.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    // main method for the class Main in file Main.java
+	public static void main(String[] args) {
+		// some code to just saying hello to the world!
+		System.out.println("Hello World");
+		System.out.println("This is Java World Fun Repo");
+	}
+}
